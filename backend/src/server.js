1138e882dc084cd2b7756a5fe3b888d9054ee00f
Andrei Stanciu <andreistanciu.ce@gmail.com>
2026-03-12 21:23:09 +0200

    assure db connection first

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,16 +8,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 app.use(express.json());
 app.use(rateLimiter);
-// app.use((req,res,next) => {
-//   console.log("We just got a new request",req.method,req.url);
-//   next();
-// })
+
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server started on PORT: ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+  });
 });
