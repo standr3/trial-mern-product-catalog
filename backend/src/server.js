ce5c1eaca10e8b23a7f8ab2e34459e97510f8640
Andrei Stanciu <andreistanciu.ce@gmail.com>
2026-03-12 21:13:18 +0200

    added redis ratelimit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+import notesRoutes from "./routes/notesRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -10,13 +11,12 @@ const PORT = process.env.PORT || 5001;
 connectDB();
 
 app.use(express.json());
-
-app.use((req,res,next) => {
-  console.log("We just got a new request",req.method,req.url);
-  next();
-})
+app.use(rateLimiter);
+// app.use((req,res,next) => {
+//   console.log("We just got a new request",req.method,req.url);
+//   next();
+// })
 app.use("/api/notes", notesRoutes);
-
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);
