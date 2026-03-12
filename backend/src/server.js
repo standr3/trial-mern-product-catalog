a24918ac6b18eecde47d0ed72e5ac40965c3cc5c
Andrei Stanciu <andreistanciu.ce@gmail.com>
2026-03-12 19:53:29 +0200

    middleware prep

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,12 @@ connectDB();
 
 app.use(express.json());
 
+app.use((req,res,next) => {
+  console.log("We just got a new request",req.method,req.url);
+  next();
+})
 app.use("/api/notes", notesRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);
