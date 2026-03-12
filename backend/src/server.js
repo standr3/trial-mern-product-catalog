698b60880057bbe409e056ea8650a0f55fcb76d5
Andrei Stanciu <andreistanciu.ce@gmail.com>
2026-03-12 19:43:15 +0200

    notes controller rewrite; added getbyid

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,7 @@
 import express from "express";
-import dotenv from "dotenv"
-import notesRoutes from "./routes/notesRoutes.js"
-import {connectDB} from "./config/db.js" 
-
+import dotenv from "dotenv";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const app = express();
@@ -10,10 +9,9 @@ const PORT = process.env.PORT || 5001;
 
 connectDB();
 
+app.use(express.json());
+
 app.use("/api/notes", notesRoutes);
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);
