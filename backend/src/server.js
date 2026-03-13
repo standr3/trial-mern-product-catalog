4e56e113309552edd973c647757c480e3ac45d5d
Andrei Stanciu <andreistanciu.ce@gmail.com>
2026-03-13 11:30:04 +0200

    home page setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import notesRoutes from "./routes/notesRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 app.use(rateLimiter);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,6 @@ import NodeDetailPage from './pages/NodeDetailPage'
 const App = () => {
   return (
     <div data-theme="forest">
-      <button className='btn btn-primary'> click me</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
