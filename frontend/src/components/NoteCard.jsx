73af9c1b6ce4a1d91897ce0ce687fd2f79fdadcf
Andrei Stanciu <andreistanciu.ce@gmail.com>
2026-03-13 12:02:20 +0200

    delete notes added

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 import { Link } from 'react-router'
 import { PenSquareIcon, Trash2Icon } from 'lucide-react'
 import { formatDate } from '../lib/utils'
+import { toast } from 'react-hot-toast'
+import api from "../lib/axios";
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
+
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+
+    try {
+      await api.delete(`/notes/${id}`);
+      setNotes((prev) => prev.filter((note) => note._id !== id)); 
+      toast.success("Note deleted successfully");
+    } catch (error) {
+      console.log("Error in handleDelete", error);
+      toast.error("Failed to delete note");
+    }
+  };
+
   return (
     <Link
       to={`/note/${note._id}`}
@@ -24,7 +42,10 @@ const NoteCard = ({ note }) => {
           <div className="flex items-center gap-1">
             <PenSquareIcon className="size-4" />
 
-            <button className="btn btn-ghost btn-xs text-error">
+            <button
+              className="btn btn-ghost btn-xs text-error"
+              onClick={(e) => handleDelete(e, note._id)}
+            >
               <Trash2Icon className="size-4" />
             </button>
           </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI'
 import { toast } from 'react-hot-toast'
 import NoteCard from '../components/NoteCard';
+import NotesNotFound from '../components/NotesNotFound';
 
 const HomePage = () => {
 
@@ -15,7 +16,7 @@ const HomePage = () => {
     const fetchNotes = async () => {
       try {
         const res = await api.get("/notes");
-        
+
         setNotes(res.data);
       } catch (error) {
         console.log("Error fetching notes");
@@ -44,10 +45,13 @@ const HomePage = () => {
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
+
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <NoteCard key={note._id} note={note} />
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
         )}
